5c42757a8e01f3aa96f7860d34179dd94bd1c2c1
Olha-Suchok <suchok_olya@ukr.net>
2022-12-28 15:41:44 +0200

    Fixed

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 import { space, layout, color } from 'styled-system';
 
 export const Box = styled('div')(space, layout, color);
